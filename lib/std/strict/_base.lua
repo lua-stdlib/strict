05918724aac88914962236dcf30179b93b59e805
Gary V. Vaughan <gary@gnu.org>
2020-04-15 16:05:04 -0700

    maint: update copyright statements for 2020.
    * LICENSE.md, Makefile, README.md, build-aux/config.ld.in,
    lib/std/strict/_base.lua, lib/std/strict/init.lua,
    spec/spec_helper.lua, spec/strict_spec.yaml,
    spec/version_spec.yaml: Add 2020 to copyright years.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/std/strict/_base.lua
+++ lib/std/strict/_base.lua
@@ -1,6 +1,6 @@
 --[[
  Strict variable declarations for Lua 5.1, 5.2, 5.3 & 5.4.
- Copyright (C) 2014-2019 std.strict authors
+ Copyright (C) 2014-2020 std.strict authors
 ]]
 --[[--
  Some minimal subset of std.normalize to make sure strict is
